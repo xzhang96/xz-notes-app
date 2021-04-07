1a016fc61ae5cb65315bbcdb8faa9cd2b8a96a4c
Cindy Zhang <cynthiacheung831@gmail.com>
2021-04-07 03:03:49 -0500

    modified Home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -6,6 +6,7 @@ import { onError } from "../libs/errorLib";
 import "./Home.css";
 import { BsPencilSquare } from "react-icons/bs";
 import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   const [notes, setNotes] = useState([]);
@@ -66,6 +67,14 @@ export default function Home() {
       <div className="lander">
         <h1>Scratch</h1>
         <p className="text-muted">A simple note taking app</p>
+        <div className="pt-3">
+          <Link to="/login" className="btn btn-info btn-lg mr-3">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
